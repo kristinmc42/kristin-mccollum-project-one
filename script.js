e0bb1c476f8cdeaa974011e585b826e98528bba5
kristinmc42 <mccollum.k.j@gmail.com>
2021-11-01 23:43:28 -0400

    created event listener for hamburger menu, toggled button icon classes, changed hamburger nav to display block

--- script.js
+++ script.js
@@ -2,4 +2,25 @@
 // when user clicks on hamburger menu icon, show hidden menu
 // when user clicks on x hide menu
 
-const 
+//get button element
+const buttonEl = document.querySelector('button.hamburger');
+
+//get hamburger nav
+const navEl = document.querySelector('nav.hamburger');
+console.log(navEl);
+
+//add event listener
+buttonEl.addEventListener('click', function(event){
+  console.log(event);
+  if (event.target.tagName === "I"){
+    // show hamburger menu and x icon, hide bars icon
+    event.target.classList.toggle('fa-bars');
+    event.target.classList.toggle('fa-times');
+    event.target.parentElement.previousElementSibling.style.display = "block";
+  }
+})
+  
+
+
+
+
