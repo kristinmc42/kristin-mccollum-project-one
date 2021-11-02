8cbb4396e803527c3670614d0f6c065008106463
kristinmc42 <mccollum.k.j@gmail.com>
2021-11-02 10:01:43 -0400

    toggled icons in hamburger menue

--- script.js
+++ script.js
@@ -6,8 +6,9 @@
 const buttonEl = document.querySelector('button.hamburger');
 
 //get hamburger nav
-const navEl = document.querySelector('nav.hamburger');
+const navEl = document.querySelector('nav.hamburger ul');
 console.log(navEl);
+
 
 //add event listener
 buttonEl.addEventListener('click', function(event){
@@ -16,7 +17,22 @@ buttonEl.addEventListener('click', function(event){
     // show hamburger menu and x icon, hide bars icon
     event.target.classList.toggle('fa-bars');
     event.target.classList.toggle('fa-times');
-    event.target.parentElement.previousElementSibling.style.display = "block";
+    console.log(event.target.className);
+
+
+    navEl
+    // event.target.parentElement.previousElementSibling.classList.toggle('hide');
+    // event.target.parentElement.previousElementSibling.classList.toggle('show');
+
+
+
+
+
+  //   if (event.target.className === 'fas fa-times')
+  //     // event.target.parentElement.previousElementSibling.style.display = "block";
+  //     navEl.style.display = 'block';
+  // } else if (event.target.className === 'fas fa-bars'){
+  //      navEl.style.display = 'none';
   }
 })
   
