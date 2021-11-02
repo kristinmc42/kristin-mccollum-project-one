6fcf7cd6dafa70a18859863fa3ddbdc5eb256bec
kristinmc42 <mccollum.k.j@gmail.com>
2021-11-02 18:32:00 -0400

    stored input values and added conditionals if fields are empty

--- script.js
+++ script.js
@@ -32,14 +32,70 @@ buttonEl.addEventListener('click', function(event){
 // get name and alert that message was recieved
 // make email address field required
 
-// get submit button for forms
-const formButtonEl = document.querySelector('form');
+// get submitted form
+const formEl = document.querySelector('form');
 
-console.log(formButtonEl)
 
-// event listener
-formButtonEl.addEventListener('submit', function(e){
+// event listener on form submit
+formEl.addEventListener('submit', function(e){
   e.preventDefault();
   console.log(e);
  
+  // get the userName from the input and save value as variable
+  const inputNameEl = document.querySelector('input[name=userName]');
+  const userName = inputNameEl.value;
+
+  // get the email address from the input and save value as variable
+  const inputEmailEl = document.querySelector('input[name=emailAddress]');
+  const userEmail = inputEmailEl.value;
+
+  // get the comment text from the input and save value as variable
+  const inputCommentText = document.querySelector('textarea');
+  const userComment = inputCommentText.value;
+
+  console.log(userName, userEmail, userComment);
+
+  if(userName && userEmail && userComment){
+    // all fields were completed
+    // alert user that info was recieved
+    alert(`Thank you ${userName} for your submission.`);
+
+    // clear the value of the form fields
+    inputNameEl.value = '';
+    inputEmailEl.value = '';
+    inputCommentText.value = '';
+  }else if (userName && userEmail && !userComment){
+    // user left comment field empty
+    const proceed = prompt('The comment field was empty. Would you like to submit it anyway? (yes / no)', 'y');
+    if (proceed){
+      // if not null convert to lower case
+      proceed.toLowerCase();
+    }
+
+    if(proceed === 'yes' || proceed === 'y'){
+      // user wishes to submit form (also default setting)
+       // alert user that info was recieved
+    alert(`Thank you ${userName} for your submission.`);
+
+    // clear the value of the form fields
+    inputNameEl.value = '';
+    inputEmailEl.value = '';
+    inputCommentText.value = '';
+    }else {
+      // user doesn't want to submit the form
+      alert('Nothing was submitted. Please complete the form.');
+    }
+  }else {
+    alert(' Nothing was submitted. Please include a name and email address');
+  };
+
+  
+
+  // OPTION - could disable the submit button *ONLY* if want user to be able to submit it only once
+  // const button = document.querySelector('button.submit');
+  
+  // button.addEventListener('click', function(){
+  //   this.disabled = true;
+  // }, {once: true})
+  
 })
